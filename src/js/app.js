83aac0426e0ab84c531a78334b1d2dd8847e5699
Patricio-Espinoza-Rojas <p.alfonsoespinoza@gmail.com>
2022-06-23 16:30:10 +0000

    modify conditional profile card

--- src/js/app.js
+++ src/js/app.js
@@ -29,18 +29,49 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let name = "Lucy";
+  if (variables.name != null) name = variables.name;
+
+  let lastname = "Boillet";
+  if (variables.lastname != null) lastname = variables.lastname;
+
+  let role = "Web Developer";
+  if (variables.role != null) role = variables.role;
+
+  let city = "Miami";
+  if (variables.city != null) city = variables.city;
+
+  let country = "USA";
+  if (variables.country != null) country = variables.country;
+
+  let socialMediaPosition = "position-right";
+  if (variables.socialMediaPosition != null)
+    socialMediaPosition = variables.socialMediaPosition;
+
+  let twitter = "twitter";
+  if (variables.twitter != null) twitter = variables.twitter;
+
+  let github = "github";
+  if (variables.github != null) github = variables.github;
+
+  let linkedin = "linkedin";
+  if (variables.linkedin != null) linkedin = variables.linkedin;
+
+  let instagram = "instagram";
+  if (variables.instagram != null) instagram = variables.instagram;
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${name} ${lastname}</h1>
+          <h2>${role}</h2>
+          <h3>${city}, ${country}</h3>
+          <ul class="${socialMediaPosition}">
+            <li><a href="https://twitter.com/4geeksacademy">${twitter}<i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/4geeksacademy">${github}<i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/4geeksacademy">${linkedin}<i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/4geeksacademy">${instagram}<i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
